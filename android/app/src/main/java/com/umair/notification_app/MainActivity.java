66d7b33ce9d47ecc3167dec32cc072e464e34e01
UMAIR <umair.nawaz1997@gmail.com>
2020-12-14 14:47:38 +0500

    Compete Notifi app

---
+++ android/app/src/main/java/com/umair/notification_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.umair.notification_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/SharedPref.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class SharedPref {
+
+  read(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    var d = json.decode(prefs.getString(key));
+    int len = d.length;
+    List<String> l = [];
+    for(int i =0; i< len; i++){
+      l.add(d[i]);
+    }
+    return l;
+  }
+
+  save(String key, value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, json.encode(value));
+  }
+
+  remove(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
+  }
+}
