6763eeb4479223873e538ea6424a7f91f0ab08f4
UMAIR <umair.nawaz1997@gmail.com>
2020-12-18 01:59:37 +0500

    change svg to png ... svg is slow to load

---
+++ lib/massage.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:sms/sms.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:csv/csv.dart';
+import 'package:notification_app/constants.dart';
+
+class massage extends StatefulWidget {
+  final List<Contact> contact;
+
+  massage({Key key, @required this.contact}) : super(key: key);
+
+  @override
+  _MessageState createState() => new _MessageState(contact);
+}
+
+class _MessageState extends State<massage> {
+  List<String> recipients = [];
+  List<Contact> contact;
+  String m;
+
+  _MessageState(this.contact);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _sendSMS() {
+    for (var i = 0; i < contact.length; i++) {
+      SmsSender sender = new SmsSender();
+      String address = contact[i].phones.toList()[0].value;
+
+      SmsMessage message = new SmsMessage(address, m);
+      sender.sendSms(message);
+      message.onStateChanged.listen((state) {
+        if (state == SmsMessageState.Sent) {
+          print("SMS is sent to " + contact[i].displayName + " !");
+        } else if (state == SmsMessageState.Delivered) {
+          print("SMS is delivered!");
+        }
+      });
+    }
+    Fluttertoast.showToast(
+        msg: "Successfully Sent!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 15.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Send Message"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 15.0,),
+          Container(
+            padding: EdgeInsets.all(15.0),
+              child: new TextField(minLines: 4,maxLines: 10,style: TextStyle(fontSize: 18.0),
+            onChanged: (text) {
+              m = text;
+            },
+            decoration: new InputDecoration(
+              border: new OutlineInputBorder(
+                  borderSide: new BorderSide(color: kPrimaryColor,width: 1.0)),
+              labelText: 'Message',
+            ),
+          )),
+          SizedBox(height: 15.0,),
+          SizedBox.fromSize(
+            size: Size(56, 56), // button width and height
+            child: ClipOval(
+              child: Material(
+                color: kPrimaryColor, // button color
+                child: InkWell(
+                  splashColor: NotificationColor, // splash color
+                  onTap: () {
+                    _sendSMS();
+                  }, // button pressed
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Icon(Icons.send_rounded,color: Colors.white,),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/sms.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:notification_app/massage.dart';
+
+
+class sms extends StatefulWidget {
+
+  final String reloadLabel = 'Reload!';
+  final String fireLabel = 'Submit';
+  final Color floatingButtonColor = Colors.red;
+  final IconData reloadIcon = Icons.refresh;
+  final IconData fireIcon = Icons.filter_center_focus;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState(
+    floatingButtonLabel: this.fireLabel,
+    icon: this.fireIcon,
+    floatingButtonColor: this.floatingButtonColor,
+  );
+}
+
+class _MyHomePageState extends State<sms> {
+  List<Contact> _contacts = new List<Contact>();
+  List<CustomContact> _uiCustomContacts = List<CustomContact>();
+  List<CustomContact> _allContacts = List<CustomContact>();
+  bool _isLoading = false;
+  bool _isSelectedContactsView = false;
+  String floatingButtonLabel;
+  Color floatingButtonColor;
+  IconData icon;
+
+  TextEditingController controller = new TextEditingController();
+
+  _MyHomePageState({
+    this.floatingButtonLabel,
+    this.icon,
+    this.floatingButtonColor,
+  });
+
+  @override
+  void initState() {
+    super.initState();
+    refreshContacts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Contacts"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.navigate_next_rounded,
+              color: Colors.white,
+              size: 40.0,
+            ),
+            onPressed: () {
+              List<Contact> mylist = [];
+              for(int i=0; i< _uiCustomContacts.length ; i++){
+                mylist .add(_uiCustomContacts[i].contact);
+              }
+              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => massage(contact: mylist)));
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Card(
+                child: new ListTile(
+                  leading: new Icon(Icons.search),
+                  title: new TextField(
+                    controller: controller,
+                    decoration: new InputDecoration(
+                        hintText: 'Search', border: InputBorder.none),
+                    onChanged: onSearchTextChanged,
+                  ),
+                  trailing: new IconButton(
+                    icon: new Icon(Icons.cancel),
+                    onPressed: () {
+                      controller.clear();
+                      onSearchTextChanged('');
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: !_isLoading
+                ? Container(
+              child: ListView.builder(
+                itemCount: _uiCustomContacts?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  CustomContact _contact = _uiCustomContacts[index];
+                  var _phonesList = _contact.contact.phones.toList();
+
+                  return _buildListTile(_contact, _phonesList);
+                },
+              ),
+            )
+                : Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton.extended(
+        backgroundColor: floatingButtonColor,
+        onPressed: _onSubmit,
+        icon: Icon(icon),
+        label: Text(floatingButtonLabel),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  void _onSubmit() {
+    setState(() {
+      if (!_isSelectedContactsView) {
+        _uiCustomContacts =
+            _allContacts.where((contact) => contact.isChecked == true).toList();
+        _isSelectedContactsView = true;
+        _restateFloatingButton(
+          widget.reloadLabel,
+          Icons.perm_identity_rounded,
+          Colors.green,
+        );
+      } else {
+        _uiCustomContacts = _allContacts;
+        _isSelectedContactsView = false;
+        _restateFloatingButton(
+          widget.fireLabel,
+          Icons.perm_identity_rounded,
+          Colors.red,
+        );
+      }
+    });
+  }
+
+  ListTile _buildListTile(CustomContact c, List<Item> list) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(
+            (c.contact.displayName[0] +
+                c.contact.displayName[1].toUpperCase()),
+            style: TextStyle(color: Colors.white)),
+      ),
+      title: Text(c.contact.displayName ?? ""),
+      subtitle: list.length >= 1 && list[0]?.value != null
+          ? Text(list[0].value)
+          : Text(''),
+      trailing: Checkbox(
+          activeColor: Colors.green,
+          value: c.isChecked,
+          onChanged: (bool value) {
+            setState(() {
+              c.isChecked = value;
+            });
+          }),
+    );
+  }
+
+  void _restateFloatingButton(String label, IconData icon, Color color) {
+    floatingButtonLabel = label;
+    icon = icon;
+    floatingButtonColor = color;
+  }
+
+  refreshContacts() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var contacts = await ContactsService.getContacts();
+    _populateContacts(contacts);
+  }
+
+  void _populateContacts(Iterable<Contact> contacts) {
+    _contacts = contacts.where((item) => item.displayName != null).toList();
+    _contacts.sort((a, b) => a.displayName.compareTo(b.displayName));
+    _allContacts =
+        _contacts.map((contact) => CustomContact(contact: contact)).toList();
+    setState(() {
+      _uiCustomContacts = _allContacts;
+      _isLoading = false;
+    });
+  }
+
+  onSearchTextChanged(String text) async {
+    // _uiCustomContacts.clear();
+    if (text.isEmpty) {
+      _uiCustomContacts = _allContacts;
+      setState(() {
+      });
+      return;
+    }
+
+    _uiCustomContacts =
+        _allContacts.where((contact) => contact.contact.displayName.toLowerCase().contains(text.toLowerCase()) ).toList();
+    setState(() {
+
+    });
+  }
+
+
+// Future<bool> getContactsPermission() =>
+//     SimplePermissions.requestPermission(Permission.ReadContacts);
+}
+
+class CustomContact {
+  final Contact contact;
+  bool isChecked;
+
+  CustomContact({
+    this.contact,
+    this.isChecked = false,
+  });
+}
+
